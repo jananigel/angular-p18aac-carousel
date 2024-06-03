1021c4fa8cf444fe36a4c6d1a46e33271dc5ad06
jananigel <jananigel@hotmail.com>
2024-06-03 18:57:19 +0800

    feat: capable of useing the mouse to change the content

--- src/app/directives/gesture-direction.directive.ts
+++ src/app/directives/gesture-direction.directive.ts
@@ -6,6 +6,7 @@ import { Directive, EventEmitter, HostListener, Output } from '@angular/core';
 export class GestureDirectionDirective {
   private swipeCoord?: [number, number];
   private swipeTime?: number;
+  private isMouseDown = false;
 
   @Output() swipeToLeft = new EventEmitter<void>();
   @Output() swipeToRight = new EventEmitter<void>();
@@ -27,6 +28,52 @@ export class GestureDirectionDirective {
       event.changedTouches[0].pageX,
       event.changedTouches[0].pageY,
     ];
+    const time = new Date().getTime();
+    const direction = [
+      coord[0] - this.swipeCoord[0],
+      coord[1] - this.swipeCoord[1],
+    ];
+    const duration = time - this.swipeTime;
+    if (
+      duration < 1000 &&
+      Math.abs(direction[0]) > 30 &&
+      Math.abs(direction[0]) > Math.abs(direction[1] * 3)
+    ) {
+      if (direction[0] < 0) {
+        //next
+        console.log('next (swipe to left)');
+        this.swipeToLeft.next();
+      } else {
+        //previous
+        console.log('previous (swipe to right)');
+        this.swipeToRight.next();
+      }
+    }
+  }
+
+  @HostListener('mousedown', ['$event'])
+  onMouseDown(event: any) {
+    console.log('mouse = ', event);
+    this.isMouseDown = true;
+    const coord: [number, number] = [event.pageX, event.pageY];
+    const time = new Date().getTime();
+    this.swipeCoord = coord;
+    this.swipeTime = time;
+  }
+
+  @HostListener('mousemove', ['$event'])
+  onMouseMove(event: any) {
+    if (!this.isMouseDown) {
+      return;
+    }
+    console.log('mouse move = ', event.pageX);
+  }
+
+  @HostListener('mouseup', ['$event'])
+  onMouseUp(event: any) {
+    this.isMouseDown = false;
+
+    const coord: [number, number] = [event.pageX, event.pageY];
     const time = new Date().getTime();
     const direction = [
       coord[0] - this.swipeCoord[0],
